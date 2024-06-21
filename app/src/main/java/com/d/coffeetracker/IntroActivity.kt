785a3419819d31c0551dee0453e6f9b9ae1c9080
Davide <69810644+ItzDavi@users.noreply.github.com>
2024-06-21 14:33:26 +0200

    Fixed crash

--- app/src/main/java/com/d/coffeetracker/IntroActivity.kt
+++ app/src/main/java/com/d/coffeetracker/IntroActivity.kt
@@ -3,6 +3,8 @@ package com.d.coffeetracker
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.d.coffeetracker.arch.Constants
+import com.d.coffeetracker.utils.MyUtils
 import com.github.appintro.AppIntro2
 import com.github.appintro.AppIntroFragment
 import com.github.appintro.AppIntroPageTransformerType
@@ -14,9 +16,13 @@ class IntroActivity : AppIntro2() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //if (MyUtils.getFromSharedPrefs(this, Constants.SP_FIRST_SETUP) == MyUtils.SharedPrefNotFound)
-
-        setupAndStart()
+        if (MyUtils.getFromSharedPrefs(this, Constants.SP_FIRST_SETUP) == MyUtils.SharedPrefNotFound) {
+            setupAndStart()
+        }
+        else {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 
     override fun onDonePressed(currentFragment: Fragment?) {
